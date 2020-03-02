72ccebe303db9e118280d65dc75ebf3e1c8c7225
paq22 <vulfel@yandex.ru>
2020-03-02 20:59:32 +0300

    bad query exapmle

--- oci8_example.rb
+++ oci8_example.rb
@@ -1,11 +1,10 @@
-# библиотека для подключения к Oracle
+# библиотека для подключения к Oracle 123
 require 'oci8'
 
 # подключение к БД
 connection = OCI8.new('C##PA', 'PAPASSWORD')
 
-# пример запроса к БД, по умолчанию создается схема,
-# название которой совпадает с именем пользователя
+# пример запроса к БД, который выполняется через Oracle, но не выполняется через Ruby
 connection.exec("SELECT * from (SELECT lastname FROM people ORDER BY 'lastname' ASC ) WHERE rownum <= 1;") do |responce|
     p responce
 end
