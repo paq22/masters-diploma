3fb4ad939edf1b4755479a2c29d37c000c276ece
paq22 <vulfel@yandex.ru>
2020-04-26 15:35:08 +0300

    theme 1 run example

--- SQLTests/lib/theme1_run.rb
+++ SQLTests/lib/theme1_run.rb
@@ -5,4 +5,9 @@
 require './theme1'
 
 theme1 = Theme1.new()
-puts theme1.theme_test()
+#puts theme1.theme_test()#.force_encoding(Encoding::CP1251).encode(Encoding::UTF_8)
+Dir.chdir("tests_themes")
+# puts Dir.pwd
+file = File.open('theme1.txt', 'w+')
+file.puts(theme1.theme_test())
+file.close()
